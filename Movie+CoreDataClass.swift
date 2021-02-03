6f93756ad19e7f251c126fa20673baf855a3cda8
Matt Sousa <mattsousatwo@gmail.com>
2021-02-03 18:55:12 -0500

    Added Movie CoreData Class

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  PopcornSwirl
+//
+//  Created by Matthew Sousa on 2/3/21.
+//  Copyright © 2021 Matthew Sousa. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
