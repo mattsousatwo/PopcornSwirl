045fb8dccdbd2e5ea4f87731e55a553e7155f3cc
Matt Sousa <mattsousatwo@gmail.com>
2021-02-04 14:25:03 -0500

    Added Movie Class to coredata

--- PopcornSwirl/MoviesStore.swift
+++ PopcornSwirl/MoviesStore.swift
@@ -16,17 +16,20 @@ class MoviesStore: ObservableObject {
     var context: NSManagedObjectContext
     var entity: NSEntityDescription
     
-    @ObservedObject private var movieStore = MovieStore()
-    
-    @Published var popularMovies = [PopMovie]()
-    @Published var upcommingMovies = [UpcomingMovie]()
-    
+    @Published var allMovies = [Movie]()
+    @Published var popularMovies = [Movie]()
+    @Published var upcomingMovies = [Movie]()
     
     init() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         context = appDelegate.persistentContainer.viewContext
         entity = NSEntityDescription.entity(forEntityName: MovieKeys.entity.rawValue, in: context)!
     }
+    
+}
+
+// MARK: SAVING
+extension MoviesStore {
     
     func saveContext() {
         do {
@@ -36,28 +39,107 @@ class MoviesStore: ObservableObject {
         }
     }
     
-    
-    func loadUpcommingAndPopularMovies() {
+    // Save a new movie
+    func saveNewMovie(uuid: Double, category: MovieCategory , director: String = "", title: String = "", overview: String = "", genres: [Int] = [], releaseDate: String = "", rating: Double = 0.0, isFavorite: Bool = false, isWatched: Bool = false, comment: String = "") -> Movie {
         
+        let movie = Movie(context: context)
+        
+        movie.uuid = uuid
+        movie.category = category.rawValue
+        
+        movie.director = director
+        movie.title = title
+        movie.overview = overview
+//        movie.genres = genres ---- CONVERT TO NSOBJECT?
+        movie.releaseDate = releaseDate
+        movie.rating = rating
+        
+        movie.isFavorite = isFavorite
+        movie.isWatched = isWatched
+        movie.comment = comment
+        
+        saveContext()
+        
+        return movie
     }
-    
-    func fetchPopularMovies() {
-        movieStore.fetchPopularMovies()
-        
-//        if movieStore.popularMovies
-        
-        
-    }
-    
-    func fetchFavoriteMovies() {
-        // filter through movie list for movies that are favorited
-    }
-    
     
 }
 
 
-enum MovieKeys: String {
-    case entity = "Movies"
+// MARK: FETCHING
+extension MoviesStore {
     
+    // Fetch all movies
+    func fetchMovies(_ movieCategory: MovieCategory = .none) {
+        let request: NSFetchRequest<Movie> = Movie.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "category = %@", movieCategory.rawValue)
+        
+        do {
+            let result = try context.fetch(request)
+            
+            switch movieCategory {
+            case .none:
+                allMovies.append(contentsOf: result)
+            case .popular:
+                popularMovies.append(contentsOf: result)
+            case .upcoming:
+                upcomingMovies.append(contentsOf: result)
+            }
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    // Extract all movies
+    func extractMovies(_ movieCategory: MovieCategory = .none) -> [Movie] {
+        var movies = [Movie]()
+        
+        switch movieCategory {
+        case .none:
+            fetchMovies()
+            movies.append(contentsOf: allMovies)
+        case .popular:
+            fetchMovies(.popular)
+            movies.append(contentsOf: popularMovies)
+        case .upcoming:
+            fetchMovies(.upcoming)
+            movies.append(contentsOf: upcomingMovies)
+        }
+        
+        return movies
+    }
+    
+    
+    
+    
+    
+    
+}
+
+// MARK: DELETING
+extension MoviesStore {
+    
+    func deleteAllMovie() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: MovieKeys.entity.rawValue)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+        do {
+            try context.execute(deleteRequest)
+        } catch {
+            print(error)
+        }
+        saveContext()
+    }
+    
+}
+
+enum MovieKeys: String {
+    case entity = "Movie"
+}
+
+enum MovieCategory: String {
+    case none = "---" // Movie is not in Popular or Upcoming Movies Scroll Bar
+    case popular = "Popular"
+    case upcoming = "Upcoming"
 }
